3f44061baeeec1af550189329de752eb94236517
Thomas Campbell <thomas.jcampbell1988@gmail.com>
2022-05-13 16:30:14 +0100

    Add animation to reveal, wip

--- app/src/main/java/self/tomc/nawc/WordleGrid.kt
+++ app/src/main/java/self/tomc/nawc/WordleGrid.kt
@@ -16,79 +16,89 @@ import androidx.compose.ui.unit.em
 import kotlinx.coroutines.delay
 import self.tomc.nawc.game.GuessState
 import self.tomc.nawc.game.Guesses
+import self.tomc.nawc.game.LetterTileState
 import self.tomc.nawc.game.TileMatchState
 
 @Composable
 fun WordleGrid(guesses: Guesses, modifier: Modifier = Modifier, revealing: GuessState? = null, onRevealedRow: () -> Unit = {}) {
-    val flipAnimationTime = 800
-
-    var rotated by remember { mutableStateOf(false) }
-    var showRevealing by remember { mutableStateOf(false) }
-    rotated = revealing != null
-    showRevealing = false
-
-    val rotation by animateFloatAsState(
-        targetValue = if (rotated) 90f else 0f,
-        animationSpec = repeatable(
-            2,
-            tween(flipAnimationTime, easing = LinearEasing),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-
-    LaunchedEffect(revealing) {
-        if(revealing != null) {
-            delay(flipAnimationTime.toLong())
-            showRevealing = true
-            delay(flipAnimationTime.toLong())
-            onRevealedRow()
-            showRevealing = false
-        }
-    }
-
     Surface(
         color = MaterialTheme.colors.background,
         modifier = modifier.then(Modifier.fillMaxWidth())
     ) {
         Column(verticalArrangement = Arrangement.Center) {
             guesses.asList().forEach { guess ->
-                val rowModifier = if(revealing != null && guess == guesses.activeGuess()) Modifier.graphicsLayer {
-                    rotationX = rotation
-                } else Modifier
-                val guessToShow = if(revealing != null && guess == guesses.activeGuess() && showRevealing) revealing else guess
-                WordleRow(guessToShow, modifier = rowModifier)
+                val revealingForRow = if(revealing != null && guess == guesses.activeGuess()) revealing else null
+                WordleRow(guess, revealing = revealingForRow, onRevealedRow = onRevealedRow)
             }
         }
     }
 }
 
 @Composable
-fun WordleRow(guess: GuessState, modifier: Modifier = Modifier, revealing: GuessState? = null) {
+fun WordleRow(guess: GuessState, modifier: Modifier = Modifier, revealing: GuessState? = null, onRevealedRow: () -> Unit = {}) {
     Row(
         horizontalArrangement = Arrangement.SpaceEvenly,
         modifier = modifier.then(Modifier.fillMaxWidth())
     ) {
-        Tile(guess.firstTile.letter, modifier = Modifier.weight(1f), tileMatchStateColors(guess.firstTile.tileMatchState))
-        Tile(guess.secondTile.letter, modifier = Modifier.weight(1f), tileMatchStateColors(guess.secondTile.tileMatchState))
-        Tile(guess.thirdTile.letter, modifier = Modifier.weight(1f), tileMatchStateColors(guess.thirdTile.tileMatchState))
-        Tile(guess.fourthTile.letter, modifier = Modifier.weight(1f), tileMatchStateColors(guess.fourthTile.tileMatchState))
-        Tile(guess.fifthTile.letter, modifier = Modifier.weight(1f), tileMatchStateColors(guess.fifthTile.tileMatchState))
+        Tile(guess.firstTile, modifier = Modifier.weight(1f), revealing = revealing?.firstTile, animationDelayMillis = 200)
+        Tile(guess.secondTile, modifier = Modifier.weight(1f), revealing = revealing?.secondTile, animationDelayMillis = 400)
+        Tile(guess.thirdTile, modifier = Modifier.weight(1f), revealing = revealing?.thirdTile, animationDelayMillis = 600)
+        Tile(guess.fourthTile, modifier = Modifier.weight(1f), revealing = revealing?.fourthTile, animationDelayMillis = 800)
+        Tile(guess.fifthTile, modifier = Modifier.weight(1f), revealing = revealing?.fifthTile, animationDelayMillis = 1000, onRevealedLetter = onRevealedRow)
     }
 }
 
 @Composable
 fun Tile(
-    letter: Char?,
+    letter: LetterTileState,
     modifier: Modifier = Modifier,
-    backgroundAndContentColors: BackgroundAndContentColors = BackgroundAndContentColors(backgroundColor = Color.White, contentColor = Color.Black)
+    revealing: LetterTileState? = null,
+    onRevealedLetter: () -> Unit = {},
+    animationDelayMillis: Int = 0
 ) {
+    val flipAnimationTime = 600
+
+    var rotated by remember { mutableStateOf(false) }
+    var showRevealing by remember { mutableStateOf(false) }
+    rotated = revealing != null
+
+    val rotation by animateFloatAsState(
+        targetValue = if (rotated) 90f else 0f,
+        animationSpec = repeatable(
+            2,
+            tween(
+                flipAnimationTime,
+                easing = LinearEasing,
+                delayMillis = animationDelayMillis
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    LaunchedEffect(revealing) {
+        if(revealing != null) {
+            delay(animationDelayMillis.toLong())
+            delay(flipAnimationTime.toLong())
+            showRevealing = true
+            delay(flipAnimationTime.toLong())
+            onRevealedLetter()
+        }
+    }
+
+    val rowModifier = if(revealing != null) Modifier.graphicsLayer {
+        rotationX = rotation
+    } else Modifier
+
+    val letterToShow = if(revealing != null && showRevealing) revealing else letter
+    val backgroundAndContentColors = tileMatchStateColors(letterToShow.tileMatchState)
+
     Surface(
         color = backgroundAndContentColors.backgroundColor,
         border = BorderStroke(2.dp, Color.Gray),
-        modifier = modifier.then(Modifier.padding(2.dp, 2.dp))
+        modifier = modifier.then(rowModifier).then(Modifier.padding(2.dp, 2.dp))
     ){
         Text(
-            text = letter?.toString() ?: "",
+            text = letterToShow.letter?.toString() ?: "",
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(0.dp, 18.dp),
